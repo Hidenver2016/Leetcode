1b039aefafad40cd202fb5f10812a8dd409e5679
Hidenver2016 <hgjiang2013@live.com>
2019-07-13 21:39:27 -0600

    Add files via upload

--- Python3.6/304-Py3-M-Range Sum Query 2D - Immutable.py
+++ Python3.6/304-Py3-M-Range Sum Query 2D - Immutable.py
@@ -42,3 +42,57 @@ class NumMatrix:
 
     def sumRegion(self, row1, col1, row2, col2):
         return self.sum[row2+1][col2+1]- self.sum[row1][col2+1] - self.sum[row2+1][col1] + self.sum[row1][col1]
+    
+    
+class NumMatrix1:#看这个
+    def __init__(self, matrix):
+        self.matrix = matrix
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                self.matrix[i][j]=self.matrix[i][j]+(self.matrix[i-1][j] if i>0 else 0)+\
+                (self.matrix[i][j-1] if j>0 else 0)-(self.matrix[i-1][j-1] if i>0 and j>0 else 0)
+    
+    def sumRegion(self, row1, col1, row2, col2):
+        return self.matrix[row2][col2]+(self.matrix[row1-1][col1-1] if row1>0 and col1>0 else 0)-\
+        (self.matrix[row2][col1-1] if col1>0 else 0)-(self.matrix[row1-1][col2] if row1>0 else 0)
+        
+class NumMatrix2:
+    def __init__(self, matrix):
+        self.matrix = matrix
+        if not matrix: return
+        n, m = len(matrix), len(matrix[0])
+        self.sum = [[0 for _ in range(m)] for _ in range(n)]
+        for i in range(n):
+            for j in range(m):
+                self.sum[i][j] = self.matrix[i][j]+(self.sum[i-1][j] if i>0 else 0)+\
+                (self.sum[i][j-1] if j>0 else 0)-(self.sum[i-1][j-1] if i>0 and j>0 else 0)
+                
+    def sumRegion(self, row1, col1, row2, col2):
+        return self.sum[row2][col2] + (self.sum[row1-1][col1-1] if row1 > 0 and col1 > 0 else 0)-\
+        (self.sum[row2][col1-1] if col1 > 0 else 0) - (self.sum[row1-1][col2] if row1 > 0 else 0)
+        
+        
+if __name__ == "__main__":
+    matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]]
+
+#    A = NumMatrix1(matrix)
+    B = NumMatrix2(matrix)
+#    print(A.sumRegion(2,1,4,3))
+    print(B.sumRegion(2,1,4,3))      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
--- Python3.6/355-Py3-M-Design Twitter.py
+++ Python3.6/355-Py3-M-Design Twitter.py
@@ -44,42 +44,40 @@ twitter.unfollow(1, 2);
 twitter.getNewsFeed(1);
 """
 
+
 import heapq
 
-class Twitter(object):
+class Twitter(object):#比较复杂，下面那个比较容易
 
     def __init__(self):
         self.time = 0
         self.tweets = {}
-        self.followee = {}
+        self.followee = {}#是个字典
         
 
     def postTweet(self, user, tweet):
         self.time += 1
-        self.tweets[user] = self.tweets.get(user, []) + [(-self.time,  tweet)]
+        self.tweets[user] = self.tweets.get(user, []) + [(-self.time,  tweet)]#用一个“-”, 解决排序问题，新的在前，与之后的heapq配合
         
         
 
     def getNewsFeed(self, user):
         h, tweets = [], self.tweets
-        people = self.followee.get(user, set()) | set([user])
+        people = self.followee.get(user, set()) | set([user])#得到自己follow的人以及自己，把这些人搞成一个并集
         for person in people:
             if person in tweets and tweets[person]:
-                time, tweet = tweets[person][-1]
+                time, tweet = tweets[person][-1]# 取出最新发布的tweet
                 h.append((time, tweet, person, len(tweets[person]) - 1))
-        heapq.heapify(h)
+        heapq.heapify(h)#按照时间来排序，
         news = []
         for _ in range(10):
             if h:
                 time, tweet, person, idx = heapq.heappop(h)
                 news.append(tweet)
-                if idx:
+                if idx:#因为之前只取了最新发布的，可能还有tweet没有取完，继续取
                     new_time, new_tweet = tweets[person][idx-1]
                     heapq.heappush(h, (new_time, new_tweet, person, idx - 1))
         return news
-        
-        
-
     def follow(self, follower, other):
         self.followee[follower] = self.followee.get(follower, set()) | set([other])
         
@@ -88,6 +86,61 @@ class Twitter(object):
     def unfollow(self, follower, other):
         if follower in self.followee:
             self.followee[follower].discard(other)
+    
+if __name__ == "__main__":
+    twitter = Twitter() 
+
+    twitter.postTweet(1, 5)
+
+
+    twitter.getNewsFeed(1)
+
+
+    twitter.follow(1, 2)
+
+
+    twitter.postTweet(2, 6)
+
+#// User 1's news feed should return a list with 2 tweet ids -> [6, 5].
+#// Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+    twitter.getNewsFeed(1)
+
+#// User 1 unfollows user 2.
+    twitter.unfollow(1, 2)
+
+#// User 1's news feed should return a list with 1 tweet id -> [5],
+#// since user 1 is no longer following user 2.
+    twitter.getNewsFeed(1)
+#        
+#        
+#
+
+
+
+
+
+
+
+import collections, itertools, heapq
+class Twitter(object):
+
+    def __init__(self):
+        self.timer = itertools.count(step=-1)
+        self.tweets = collections.defaultdict(collections.deque)
+        self.followees = collections.defaultdict(set)
+
+    def postTweet(self, userId, tweetId):
+        self.tweets[userId].appendleft((next(self.timer), tweetId))
+
+    def getNewsFeed(self, userId):
+        tweets = heapq.merge(*(self.tweets[u] for u in self.followees[userId] | {userId}))#新闻都合并起来，注意这个merge是从小到大自动的
+        return [t for _, t in itertools.islice(tweets, 10)]#切片取前面十个，有个一个default的初始值，是 0
+
+    def follow(self, followerId, followeeId):
+        self.followees[followerId].add(followeeId)
+
+    def unfollow(self, followerId, followeeId):
+        self.followees[followerId].discard(followeeId)
             
             
             
